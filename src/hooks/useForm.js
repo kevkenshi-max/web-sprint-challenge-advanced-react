b7a52236d9e1f9f3b9538f1147894e8d12640b95
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2021-12-03 09:42:45 -0800

    custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,9 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue);
+    return [values, setValues];
+}
+
+export default useForm;
